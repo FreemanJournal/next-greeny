e74bcfed21872cd31b3b0cc4a0ae4961819b2369
Md Ishaque <iamishaque.business@gmail.com>
2022-03-20 18:03:07 +0600

    add item to cart successfully

--- context/AppReducer.js
+++ context/AppReducer.js
@@ -2,9 +2,15 @@ export const initialState = [];
 
 export const AppReducer = (state, action) => {
     switch (action.type) {
-        case 'add_product':{
-            return [...state,action.value]
-        }
-       
+        case 'add_product':
+            if(state.find(item => item.serial === action.value.serial)){
+                return state?.map(item => item?.serial === action?.value?.serial ? { ...item, ...(++item.quantity)}:item)
+            }else{
+                return [...state,action.value]
+            }
+           
+        default:
+            return state
+
     }
 }
